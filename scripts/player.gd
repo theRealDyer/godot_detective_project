8b0f259207029dd11f0905a158de3a201aeb0ca6
Jordan Barber <jordanb421@outlook.com>
2023-12-06 23:27:30 +0000

    Finished basic player controller
    The player character can now move around on arrow key presses
    currently there is no animation for the movement and the image
    simpuy slides around the sceene. But it is bound by the screen
    size.

---
+++ scripts/player.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+@export var speed = 400 # Sets how fast player moves [pixels/sec]
+var screen_size # Size of game window
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # Player movement vector
+	if Input.is_action_pressed("walk_right"):
+		velocity.x+=1
+	if Input.is_action_pressed("walk_left"):
+		velocity.x-=1
+	if Input.is_action_pressed("walk_forward"):
+		velocity.y-=1
+	if Input.is_action_pressed("walk_backward"):
+		velocity.y+=1
+		
+	if velocity.length() > 0:
+		# Ensure that the velocity is the same diagonally as on grid
+		velocity = velocity.normalized()  * speed
+		
+	position+=velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size) # keeps player on the screen
+	
