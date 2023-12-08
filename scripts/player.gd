1a63a60ff3529900f23430e1d45b17038416d392
Jordan Barber <jordanb421@outlook.com>
2023-12-08 21:26:04 +0000

    Basic animation working
    Animation for moving left right and up down works.
    I need to rotate the animation for moving along the diagonal.
    
    Image is changed to simple pixel drawing of hat with feet.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends Area2D
-# TESTING
-@export var speed = 400 # Sets how fast player moves [pixels/sec]
+
+@export var speed = 200 # Sets how fast player moves [pixels/sec]
 var screen_size # Size of game window
 
 # Called when the node enters the scene tree for the first time.
@@ -23,6 +23,24 @@ func _process(delta):
 	if velocity.length() > 0:
 		# Ensure that the velocity is the same diagonally as on grid
 		velocity = velocity.normalized()  * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	
+	# Check direction of movenent and play correct animation
+	if velocity.y!=0: # UpDown movement
+		# Set animation to up down animation
+		$AnimatedSprite2D.animation = 'walkUD'
+		$AnimatedSprite2D.flip_h = false # Dont flip along x=0
+		#  Only flip animation if moving down
+		$AnimatedSprite2D.flip_v = velocity.y>0 
+		
+	elif velocity.x!=0: # RL movement
+		# Set correct animation
+		$AnimatedSprite2D.animation = 'walkRL'
+		$AnimatedSprite2D.flip_v = false
+		# Only flip if moving left
+		$AnimatedSprite2D.flip_h = velocity.x<0
 		
 	position+=velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size) # keeps player on the screen
