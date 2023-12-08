43073aef92bc5a7df28b441226d7f98fda9d6b9f
Jordan Barber <jordanb421@outlook.com>
2023-12-08 23:43:17 +0000

    Fixing collision with the player
    I have made the player a character2D node now so collisions are easy
    there is now a table sprite which is sectioned as a static2Dbody.
    
    An instance of the player and the table now get generated via the "main"
    script which is attached to the "level 1" scene. Click play on main
    screen to start playing with the character.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends CharacterBody2D
 
 @export var speed = 200 # Sets how fast player moves [pixels/sec]
 var screen_size # Size of game window
@@ -7,19 +7,22 @@ var screen_size # Size of game window
 func _ready():
 	screen_size = get_viewport_rect().size
 
+func start(pos):
+	# Sets initial player positon
+	position = pos
+	show()
+	
+func get_input():
+	# Gets the input buttons and makes a velocity
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_dir * speed
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.ZERO # Player movement vector
-	if Input.is_action_pressed("walk_right"):
-		velocity.x+=1
-	if Input.is_action_pressed("walk_left"):
-		velocity.x-=1
-	if Input.is_action_pressed("walk_forward"):
-		velocity.y-=1
-	if Input.is_action_pressed("walk_backward"):
-		velocity.y+=1
-		
+
+func _physics_process(delta):
+	get_input()
+	move_and_collide(velocity*delta)
+	
+	# Starts/stops animations
 	if velocity.length() > 0:
 		# Ensure that the velocity is the same diagonally as on grid
 		velocity = velocity.normalized()  * speed
@@ -41,7 +44,4 @@ func _process(delta):
 		$AnimatedSprite2D.flip_v = false
 		# Only flip if moving left
 		$AnimatedSprite2D.flip_h = velocity.x<0
-		
-	position+=velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size) # keeps player on the screen
-	
+
