46b0dcec612d7124f20f0204aabea0ebd29cd3d8
theRealDyer <54688154+theRealDyer@users.noreply.github.com>
2023-12-13 15:44:46 +0000

    added Escape function to main.gpd script (level 1) so you can exit back to main menu when playing game

--- scripts/main.gd
+++ scripts/main.gd
@@ -6,6 +6,8 @@ var screen_size = Vector2()# Size of game window
 var placed_tables = [] # Keep track of placed tables
 var player_start_pos = Vector2()
 
+var finished := false # for use when exiting level
+
 func ValidPos(pos, table):
 	var col_shape = table.get_node("CollisionShape2D")
 	var shape = col_shape.shape
@@ -64,3 +66,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func finish(): # finishes the scene and moves to the next one
+	if not finished:
+		finished = true
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func _unhandled_input(event): # calls finish when Esc is pressed
+	if event.is_action_pressed("ui_cancel"):
+		finish()
