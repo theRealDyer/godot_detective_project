4a9d087c956224e38cac42ad9be4e227e057c2a2
Jordan Barber <jordanb421@outlook.com>
2023-12-21 14:00:33 +0000

    Pop up inventory gui on press
    These commits show a working inventory gui that can be opened and closed
    by pressing tab. There includes the inventory and slot scenes as well as
    basic logic for the inventory toggling.
    
    At this stage the inventory doesn't do anything except show you the
    inventory.

---
+++ scenes/ControlGUIs.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+@onready var inventory = $Inventory
+
+func _ready():
+	# Default to closed inventory
+	inventory.Close()
+
+func _input(event):
+	if event.is_action_pressed("toggle_inventory"):
+		# If inventory button pressed, toggle Inventory
+		if inventory.isOpen:
+			inventory.Close()
+		else:
+			inventory.Open()
