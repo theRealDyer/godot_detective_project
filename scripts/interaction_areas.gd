db8619809475e227079f0cd18f6eb0cbe3efffa6
Jordan Barber <jordanb421@outlook.com>
2024-04-07 23:21:35 +0100

    Added image to pop up and fix inv
    *Item popup now works and looks nice with an image of the item.
    *The inventory system is functional, press e to pickup the item.

--- scripts/interaction_areas.gd
+++ scripts/interaction_areas.gd
@@ -1,13 +1,23 @@
 extends Area2D
 
 @onready var ui_layer = $"../UILayer/InfoPanel"
+@onready var inventory: InventoryData = preload("res://inventory/inventory.tres")
+
+
 var can_pick_up = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Connect the areas "body entered" signal to player movement
 	connect("body_entered",_on_body_entered)
 	connect("body_exited", _on_body_exited)
-	
+
+func _input(event):
+	if can_pick_up and event.is_action_pressed('pickup'):
+		# Press e to pick up item if player in the interaction area
+		inventory.insert(get_meta("Information")) # Insert Item data
+		inventory.updated.emit() # Update GUI
+		queue_free()
+		
 func _on_body_entered(body):
 	# Check if the body is the player and enable item pickup
 	if body.is_in_group("Player"): # Make sure your player node is in the 'Player' group
@@ -24,13 +34,19 @@ func _on_body_exited(body):
 func _display_Item_Info(item_info):
 	# Show resource info on item interact
 	# Access the ui for item interaction
-	var name_label = ui_layer.get_node("ColorBackground/Panel/ItemName") 
-	var description_label = ui_layer.get_node("ColorBackground/Panel/ItemDescription")
+	var name_label = ui_layer.get_node("Panel/Text/ItemName") 
+	var description_label = ui_layer.get_node("Panel/Text/ItemDescription")
+	var item_image = ui_layer.get_node("Panel/Sprite/Image")
 	
-	# Set the name and description text to that which is in the item resource
+	# Set the name and description text to that which is "in t"he item resource
 	name_label.text = item_info.name
 	description_label.text = item_info.description
 	
+	# Set image to item image if it exists
+	if item_info.texture:
+		item_image.texture = item_info.texture
+	
+
 	# Show UI elements if they are not yet visible
 	ui_layer.visible = true
 	name_label.show()
