b25e4ebac9f3ace33d9aa6703bc03f04d942f6c4
theRealDyer <54688154+theRealDyer@users.noreply.github.com>
2024-04-11 23:47:10 +0100

    added intro message before prologue. Expanded prologue map a bit.

---
+++ scenes/game_intro_message.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var finished := false
+
+
+func _ready():
+	var label1993 = $Label2_1993
+	$AnimationPlayer.play("fade_in_1")
+	await get_tree().create_timer(17).timeout
+	$AnimationPlayer.play("fade_out_1")
+	await get_tree().create_timer(3).timeout
+	get_tree().change_scene_to_file("res://scenes/introLevel.tscn")
+
+func finish(): # finishes the scene and moves to the next one
+	if not finished:
+		finished = true
+		get_tree().change_scene_to_file("res://scenes/introLevel.tscn")
+
+func _unhandled_input(event): # calls finish() when ...???
+	if event.is_action_pressed("ui_cancel")	or event.is_action_pressed("ui_accept"):
+		finish()
