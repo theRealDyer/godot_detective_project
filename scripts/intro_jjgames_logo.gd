0bd791d308eb06d01b23398dfed3620d5378f05a
theRealDyer <54688154+theRealDyer@users.noreply.github.com>
2024-04-11 02:56:29 +0100

    added sound to jj logo intro.

--- scripts/intro_jjgames_logo.gd
+++ scripts/intro_jjgames_logo.gd
@@ -6,10 +6,11 @@ func _ready():
 
 	$AnimationPlayer.play("fade_in")
 	await get_tree().create_timer(3).timeout
+	$AudioStreamPlayer.play()
 	$AnimationPlayer.play("vhs glitch")
 	await get_tree().create_timer(4).timeout
 	$AnimationPlayer.play("fade_out")
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(2).timeout
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func finish(): # finishes the scene and moves to the next one
